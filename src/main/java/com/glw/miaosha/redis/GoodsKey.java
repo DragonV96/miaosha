8b27cdbadfdb4adc2f1d1f26b4caa56e9ff1999f
DragonV96 <510537547@qq.com>
2019-05-24 00:45:16 +0800

    添加goodsKey，TODO

---
+++ src/main/java/com/glw/miaosha/redis/GoodsKey.java
@@ -0,0 +1,14 @@
+package com.glw.miaosha.redis;
+
+/**
+ * Create by glw
+ * 2019/5/20 9:14
+ */
+public class GoodsKey extends BasePrefix {
+    public GoodsKey(String prefix) {
+        super(prefix);
+    }
+
+    public static GoodsKey getGoodsList = new GoodsKey("gI");
+
+}
