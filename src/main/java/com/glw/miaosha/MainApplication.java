9a40bdd575c115b8c944a08444513aed239346d0
DragonV96 <510537547@qq.com>
2019-05-24 00:43:14 +0800

    修改为打jar包

--- src/main/java/com/glw/miaosha/MainApplication.java
+++ src/main/java/com/glw/miaosha/MainApplication.java
@@ -10,18 +10,18 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
  * @date 2019/4/30 9:53
  */
 @SpringBootApplication
-public class MainApplication extends SpringBootServletInitializer {
+public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
     }
 
-    /**
-     * 打war包---程序主入口
-     * @param builder
-     * @return
-     */
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(MainApplication.class);
-    }
+//    /**
+//     * 打war包---程序主入口
+//     * @param builder
+//     * @return
+//     */
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(MainApplication.class);
+//    }
 }
--- src/main/java/com/glw/miaosha/controller/GoodsController.java
+++ src/main/java/com/glw/miaosha/controller/GoodsController.java
@@ -1,20 +1,22 @@
 package com.glw.miaosha.controller;
 
 import com.glw.miaosha.doman.MsUser;
+import com.glw.miaosha.redis.GoodsKey;
 import com.glw.miaosha.redis.RedisService;
 import com.glw.miaosha.service.GoodsService;
 import com.glw.miaosha.service.MsUserService;
 import com.glw.miaosha.service.UserService;
 import com.glw.miaosha.vo.GoodsVo;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.thymeleaf.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.spring4.context.SpringWebContext;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
@@ -35,13 +37,31 @@ public class GoodsController {
     @Autowired
     GoodsService goodsService;
 
-    @RequestMapping("/to_list")
-    public String list(Model model,MsUser user) {
+    @Autowired
+    ThymeleafViewResolver thymeleafViewResolver;
+
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @RequestMapping(value = "/to_list", produces = "text/html")
+    @ResponseBody
+    public String list(HttpServletRequest request, HttpServletResponse response, Model model, MsUser user) {
         model.addAttribute("user", user);
         //查询商品列表
         List<GoodsVo> goodsList = goodsService.listGoodsVo();
         model.addAttribute("goodsList", goodsList);
-        return "goods_list";
+
+        // 取缓存
+        String html = redisService.get(GoodsKey.getGoodsList, "", String.class);
+        if (!StringUtils.isEmpty(html)) {
+            return html;
+        }
+        SpringWebContext ctx = new SpringWebContext(request, response, request.getServletContext(), request.getLocale(), model.asMap(), applicationContext);
+        html = thymeleafViewResolver.getTemplateEngine().process("goods_list", ctx);
+        if (!StringUtils.isEmpty(html)) {
+            redisService.set(GoodsKey.getGoodsList, "", html);
+        }
+        return html;
     }
 
     @RequestMapping("/to_detail/{goodsId}")
